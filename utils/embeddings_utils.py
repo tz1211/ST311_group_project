319f57084bc26586d80505c8306d3748088a7317
tz1211 <terryzhou1211@gmail.com>
2025-04-30 19:34:37 +0100

    add util function for token count

--- utils/embeddings_utils.py
+++ utils/embeddings_utils.py
@@ -1,12 +1,18 @@
 import logging
+from transformers import AutoTokenizer
 from FlagEmbedding import BGEM3FlagModel
 
 logging.getLogger("transformers").setLevel(logging.ERROR)
 
 embed_model_instance = BGEM3FlagModel("BAAI/bge-m3", use_fp16=True)
+DEFAULT_TOKENIZER = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-72B-Instruct", trust_remote_code=True)
 
 def compute_text_embedding(text: str, embed_model: str = embed_model_instance): 
     """
     This function takes a text and returns an embedding for the text. 
     """
     return [embed_model.encode(text)["dense_vecs"].tolist()]
+
+
+def count_tokens(text: str, tokenizer: AutoTokenizer = DEFAULT_TOKENIZER):
+    return len(tokenizer.encode(text))
