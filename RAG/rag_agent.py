6e107a72db9d453a0cdec901fdd02760f2459164
tz1211 <terryzhou1211@gmail.com>
2025-04-30 19:34:47 +0100

    tweak rag backend

--- RAG/rag_agent.py
+++ RAG/rag_agent.py
@@ -1,3 +1,4 @@
+import json
 import logging 
 import pathlib
 import pandas as pd
@@ -6,7 +7,7 @@ from openai_messages_token_helper import build_messages
 from FlagEmbedding import BGEM3FlagModel
 
 from .rag_searcher import RAGSearcher
-from utils.embeddings_utils import compute_text_embedding
+from utils.embeddings_utils import compute_text_embedding, count_tokens
 
 logger = logging.getLogger("rag_agent")
 
@@ -31,7 +32,7 @@ class RAGAgent:
         current_dir = pathlib.Path(__file__).parent
         self.system_prompt = open(current_dir / "prompts/system_prompt.txt").read()
 
-    def generate_response(self, user_query: str, df_context: pd.DataFrame, question_id: str, top: int = 3): 
+    def generate_response_rag(self, user_query: str, df_context: pd.DataFrame, question_id: str, top: int = 3): 
         logger.info(f"Entering vector search with query text: {user_query}")
         q_vector = compute_text_embedding(
             user_query,
@@ -39,14 +40,17 @@ class RAGAgent:
         )
 
         search_results = self.searcher.search(user_query, q_vector, df_context, question_id, top)
+        augmented_query = "Context:\n" + search_results + "\n" + user_query
 
         messages = build_messages(
             model=self.chat_model,
             system_prompt=self.system_prompt,
-            new_user_content=user_query + "\n\nSources:\n" + search_results,
+            new_user_content=augmented_query,
             max_tokens=self.max_tokens, 
             fallback_to_default=True,
         )
+
+        # logger.info(f"Input query: {augmented_query}")
 
         chat_completion_response = self.chat_client.chat.completions.create(
             model=self.chat_model,
@@ -54,6 +58,41 @@ class RAGAgent:
             temperature=self.temperature,
             max_tokens=self.max_tokens,
             stream=False,
+            response_format={"type": "json_object"}
         )
 
-        return chat_completion_response.choices[0].message.content
+        input_tokens = count_tokens(augmented_query, self.embed_client.tokenizer)
+
+        logger.info(f"Raw Response: {chat_completion_response.choices[0].message.content}")
+
+        response_json = json.loads(chat_completion_response.choices[0].message.content)
+        return response_json["answer"], input_tokens
+    
+    def generate_response_lc(self, user_query: str, context: str): 
+        augmented_query = "Context:\n" + context + "\n" + user_query
+        
+        messages = build_messages(
+            model=self.chat_model,
+            system_prompt=self.system_prompt,
+            new_user_content=augmented_query,
+            max_tokens=self.max_tokens, 
+            fallback_to_default=True,
+        )
+
+        # logger.info(f"Input query: {augmented_query}")
+
+        chat_completion_response = self.chat_client.chat.completions.create(
+            model=self.chat_model,
+            messages=messages,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            stream=False,
+            response_format={"type": "json_object"}
+        )
+
+        input_tokens = count_tokens(augmented_query, self.embed_client.tokenizer)
+
+        logger.info(f"Raw Response: {chat_completion_response.choices[0].message.content}")
+
+        response_json = json.loads(chat_completion_response.choices[0].message.content)
+        return response_json["answer"], input_tokens
